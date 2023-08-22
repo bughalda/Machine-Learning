b2d162316d76ac9dbc30da6b34ebf90b3bc8c32d
ALDA <122581227+AldahirLopezNavarrete@users.noreply.github.com>
2023-08-21 22:35:05 -0600

    Create GenderPrediction.r

---
+++ GenderPrediction.r
@@ -0,0 +1,49 @@
+library(tidyverse)
+library(caret)
+library(dslabs)
+data(heights)
+
+# define the outcome and predictors
+y <- heights$sex
+x <- heights$height
+
+# generate training and test sets
+set.seed(2007)
+test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+test_set <- heights[test_index, ]
+train_set <- heights[-test_index, ]
+
+# guess the outcome
+y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE) %>% 
+  factor(levels = levels(test_set$sex))
+
+# compute accuracy
+mean(y_hat == test_set$sex)
+
+# compare heights in males and females in our data set
+heights %>% group_by(sex) %>% summarize(mean(height), sd(height))
+
+# now try predicting "male" if the height is within 2 SD of the average male
+y_hat <- ifelse(x > 62, "Male", "Female") %>% factor(levels = levels(test_set$sex))
+mean(y == y_hat)
+
+# examine the accuracy of 10 cutoffs
+cutoff <- seq(61, 70)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  mean(y_hat == train_set$sex)
+})
+data.frame(cutoff, accuracy) %>% 
+  ggplot(aes(cutoff, accuracy)) + 
+  geom_point() + 
+  geom_line() 
+max(accuracy)
+
+best_cutoff <- cutoff[which.max(accuracy)]
+best_cutoff
+
+y_hat <- ifelse(test_set$height > best_cutoff, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+y_hat <- factor(y_hat)
+mean(y_hat == test_set$sex)
